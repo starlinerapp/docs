aef5e2a0bc711b20fb75ebf1c4d1cbc580557737
Rafael Neher <rafineher@gmail.com>
2026-04-21 15:12:54 +0200

    adjust config url docusaurus

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://starliner.app',
+  url: 'https://starlinerapp.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docs/',
