e49c8135c64ab26277286a62b77a78a04d3272c3
Leon Liang <hello@leonliang.lu>
2026-04-26 23:46:13 +0200

    change production url to docs.starliner.dev

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://starlinerapp.github.io',
+  url: 'https://docs.starliner.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
