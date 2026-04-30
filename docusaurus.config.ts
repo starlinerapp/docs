2817f136014c6dbebe9fd45b04fffd63ca2486ec
Leon Liang <hello@leonliang.lu>
2026-04-30 15:02:49 +0200

    fix broken links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -86,7 +86,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/intro',
+              to: '/quickStart',
             },
           ],
         },
